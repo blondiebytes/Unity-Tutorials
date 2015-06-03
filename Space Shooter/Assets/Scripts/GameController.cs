9f4f56c72e7c2a8f4c2a0e077887982e06d49464
Kathryn Hodge <kahodge@vassar.edu>
2015-06-03 11:26:41 -0400

    Main Gameplay for Space Shooter Finished

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Main task: spawning the hazards in our game
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	void Start() 
+	{
+		StartCoroutine (SpawnWaves ());
+	}
+	
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds(startWait);
+		// infinite loop of spawning waves
+		while(true) 
+		{
+			// a wave of 10 hazards
+			for (int i = 0; i < hazardCount; i++) 
+			{
+				Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation =  Quaternion.identity;
+				// Instantiate hazard
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+		}
+	}
+
+}
---
+++ Space Shooter/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+	// holds max tumble value
+	public float tumble;
+
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+
+	
+}
