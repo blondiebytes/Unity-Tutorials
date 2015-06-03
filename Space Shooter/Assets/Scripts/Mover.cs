1e00fc41bb9dc4901dce1bc0199776bd974cabe0
Kathryn Hodge <kahodge@vassar.edu>
2015-06-03 10:32:28 -0400

    Shooting Lasers in Space Shooter

---
+++ Space Shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour 
+{
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+
+		rb.velocity = transform.forward * speed;
+	}
+
+}
---
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary 
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn; // use transform instead of object.transform
+
+	public float fireRate;
+	private float nextFire;
+
+	private Rigidbody rb;
+	
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// shooting lasers
+	void Update() {
+		if (Input.GetButton("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			/*GameObject clone =  because we don't need a reference*/
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+// moving plane via forces
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.velocity = movement * speed;
+
+		// constraining the plane to the game area via clamp
+		rb.position = new Vector3
+			(
+				Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+				0.0f,
+				Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+		);
+	
+		// tilting as we move
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
